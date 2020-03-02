5422d8798ca297f1e8715ad62fe2118188c50731
angiewambui <wambuinjenga44@gmail.com>
2020-03-01 20:14:20 -0500

    added place order function

--- js/script.js
+++ js/script.js
@@ -186,8 +186,6 @@ $(document).ready(function(){
       if ($("input#name").val() && $("input#phone").val() && $("input#location").val()!=""){
   
         $("#finallmessage").append(person+", We have recieved your order and it will be delivered to you at "+location+ ". Prepare sh. "+deliceryamount);
-        $("#totalbill").hide();
-        $("#finallmessage").slideDown(1200);
       }
       else {
         alert("Please fill in the details for delivery!");
